f6540defe6f5e5d4d42a3222e7576679621493fa
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-07-02 09:15:31 -0600

    fixing links for Instagram and WhatsApp icons

--- src/components/icons/instagramIcon.jsx
+++ src/components/icons/instagramIcon.jsx
@@ -3,9 +3,11 @@ import { FaInstagram } from 'react-icons/fa';
 import { Button } from "@/components/ui/button";  // Asegúrate de importar tu componente de botón correctamente
 
 const InstagramButtonIcon = () => (
-  <Button className="w-10 h-10 flex items-center justify-center rounded-full bg-gradient-to-r from-purple-400 to-pink-500 hover:from-pink-500 hover:to-purple-400">
-    <FaInstagram className="text-white" />
-  </Button>
+  <a href="https://www.instagram.com/harmonywellnessandspa/" target="_blank" rel="noopener noreferrer">
+    <Button className="w-10 h-10 flex items-center justify-center rounded-full bg-gradient-to-r from-purple-400 to-pink-500 hover:from-pink-500 hover:to-purple-400">
+      <FaInstagram className="text-white" />
+    </Button>
+   </a>
 );
 
 export default InstagramButtonIcon;
--- src/components/icons/whatsappIcon.jsx
+++ src/components/icons/whatsappIcon.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 import { Button } from "@/components/ui/button";  // Asegúrate de importar tu componente de botón correctamente
 
 const WhatsAppIcon = () => (
-  <a href="https://wa.link/3hmdoh" target="_blank" rel="noopener noreferrer">
+  <a href="https://wa.me/18134686878" target="_blank" rel="noopener noreferrer">
     <Button className="w-10 h-10 flex items-center justify-center rounded-full bg-green-500 hover:bg-green-600">
       <FaWhatsapp className="text-white" />
     </Button>
