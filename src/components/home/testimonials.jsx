7d68bf647ac2fe1609f71e1fc037f82bb58041e3
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-07-04 15:01:10 -0600

    Debugging

--- src/components/home/testimonials.jsx
+++ src/components/home/testimonials.jsx
@@ -9,6 +9,34 @@ export default function CarouselDefault() {
     <div className="p-2 justify-center items-center">
       <h2 className="text-xl font-bold text-center text-gray-800 my-8">What Our Clients Say</h2>
       <Carousel className="rounded-xl ">
+      <div className="relative h-full w-full">
+          <img
+            src={Test2}
+            alt="image 2"
+            className="h-80 w-full object-cover"
+          />
+          <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+            <div className="w-3/4 text-center md:w-2/4">
+              <Typography
+                variant="h2"
+                color="white"
+                className="mb-4 lg:text-xl md:text-lg sm:text-base text-xl"
+              >
+                "Lynn McHugh."
+              </Typography>
+              <Typography
+                variant="lead"
+                color="white"
+                className="mb-5 lg:text-xl md:text-lg sm:text-base text-base opacity-80"
+              >
+                "Nancy is a fantastic therapist skilled in Therapeutic massage, cupping, and hot stone massage.
+                Her knowledge and nurturing nature helped me feel better. Her great smile and care kept me coming back.
+                I'm grateful we met!"
+              </Typography>
+              <Rating className="mb-10" value={5} readonly />
+            </div>
+          </div>
+        </div>
         <div className="relative h-full w-full">
           <img
             src={Test1}
@@ -37,34 +65,7 @@ export default function CarouselDefault() {
             </div>
           </div>
         </div>
-        <div className="relative h-full w-full">
-          <img
-            src={Test2}
-            alt="image 2"
-            className="h-80 w-full object-cover"
-          />
-          <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
-            <div className="w-3/4 text-center md:w-2/4">
-              <Typography
-                variant="h2"
-                color="white"
-                className="mb-4 lg:text-xl md:text-lg sm:text-base text-xl"
-              >
-                "Lynn McHugh."
-              </Typography>
-              <Typography
-                variant="lead"
-                color="white"
-                className="mb-5 lg:text-xl md:text-lg sm:text-base text-base opacity-80"
-              >
-                "Nancy is a fantastic therapist skilled in Therapeutic massage, cupping, and hot stone massage.
-                Her knowledge and nurturing nature helped me feel better. Her great smile and care kept me coming back.
-                I'm grateful we met!"
-              </Typography>
-              <Rating className="mb-10" value={5} readonly />
-            </div>
-          </div>
-        </div>
+        
         <div className="relative h-full w-full">
           <img
             src={Test3}
--- src/components/icons/facebookIcon.jsx
+++ src/components/icons/facebookIcon.jsx
@@ -3,9 +3,11 @@ import { FaFacebookF } from 'react-icons/fa';
 import { Button } from "@/components/ui/button";  // Asegúrate de importar tu componente de botón correctamente
 
 const FacebookIcon = () => (
-  <Button className="w-10 h-10 flex items-center justify-center rounded-full bg-blue-500 hover:bg-blue-600">
-    <FaFacebookF className="text-white" />
-  </Button>
+  <a href="https://www.facebook.com/profile.php?id=61562096123027" target="_blank" rel="noopener noreferrer">
+    <Button className="w-10 h-10 flex items-center justify-center rounded-full bg-blue-500 hover:bg-blue-600">
+      <FaFacebookF className="text-white" />
+    </Button>
+  </a>
 );
 
 export default FacebookIcon;
