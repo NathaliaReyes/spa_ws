0c615eb8d84a531d62ace041360deb535902978e
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-08-05 17:18:09 -0600

    Updates

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ContactForm from '../components/ContactForm';
+import bgContact from '../assets/home/bg-contact.jpeg';
 
 const Contact = () => {
   return (
-    <div>
-      <ContactForm />
+    <div className="relative">
+      <div 
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: `url(${bgContact})`, opacity: 0.7 }}
+      ></div>
+      <div className="relative pt-8 pb-8">
+        <ContactForm />
+      </div>
     </div>
   )
 }
